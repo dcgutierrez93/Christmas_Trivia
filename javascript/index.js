cecf5d17b644cf57f4345c952d85f3394fc954f5
David Gutierrez <dcgutierrez93@yahoo.com>
2017-12-11 11:16:30 -0600

    Initial Index.html setup

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+function displayLogin() {
+    $(".trivia_signup_wrapper").fadeOut(200);
+    $(".trivia_login_wrapper").fadeOut(1000);
+
+}
+
+function displaySingup() {
+    $(".trivia_signup_wrapper").fadeOut(200);
+    $(".trivia_login_wrapper").fadeOut(1000);
+}
+
+$(document).ready(function functionName() {
+    displaySingup();
+});
